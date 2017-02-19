a26479c57ac3d4adc1506f258060b3719dbb3a0a
David Kim <dhkim1211@gmail.com>
2017-02-19 10:41:56 -0800

    set up endpoint to retrieve mars rover photos

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
 
+router.get('/images', (req, res, next) => {
+  request({
+  	method: 'GET',
+  	url: `https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos`,
+  	qs: {
+  		page: req.query.page,
+  		api_key: 'YnVJc28KZxxKGJt6Tw2IUM3d8CMez3YE1L4YiSH4',
+  		sol: req.query.sol,
+  		camera: 'PANCAM'
+  	},
+  	json: true
+  }, (err, response, body) => {
+		if (err) throw err;
+		console.log('body', body);
 
+		// Remove unnecessary info about rover
+		body.photos.forEach(function(photoObject) {
+			delete photoObject.rover;
+		})
+
+		res.send(body.photos);
+  })
+})
 
 module.exports = router;
